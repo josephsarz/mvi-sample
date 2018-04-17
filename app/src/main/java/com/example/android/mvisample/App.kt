53f39bc53d4de4f6ae6c322d7eadea42b59c8b1b
Idorenyin Obong <idee4ril@gmail.com>
2018-04-17 02:30:51 +0100

    added timber for logging

--- app/src/main/java/com/example/android/mvisample/App.kt
+++ app/src/main/java/com/example/android/mvisample/App.kt
@@ -5,6 +5,7 @@ import android.app.Application
 import com.example.android.mvisample.di.DaggerAppComponent
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
+import timber.log.Timber
 import javax.inject.Inject
 
 class App:Application() , HasActivityInjector {
@@ -15,6 +16,9 @@ class App:Application() , HasActivityInjector {
     override fun onCreate() {
         super.onCreate()
         DaggerAppComponent.builder().application(this).build().inject(this)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 
     override fun activityInjector(): DispatchingAndroidInjector<Activity> {
